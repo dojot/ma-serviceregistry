3f86a6ce18be546234a7b8e969f4ef0f1385efb7
Henrique Canto Duarte <hcantoduarte@gmail.com>
2017-12-15 13:39:23 -0200

    Removed unwanted code

--- Java/ServiceRegistry/ServiceRegistry-ejb/src/main/java/br/com/dojot/mutualauthentication/serviceregistry/service/impl/LoggingServiceImpl.java
+++ Java/ServiceRegistry/ServiceRegistry-ejb/src/main/java/br/com/dojot/mutualauthentication/serviceregistry/service/impl/LoggingServiceImpl.java
@@ -11,20 +11,9 @@ import br.com.dojot.mutualauthentication.serviceregistry.service.api.LoggingServ
 
 @Stateless
 public class LoggingServiceImpl implements LoggingService {
-	
-	//@EJB
-	//private LoggingProcessingProducerService loggingProcessingProducerService; 
 
 	@Override
 	public void saveLogging(Level level, String component, String username, String details) {
-//		CommunicationFacade facade = new CommunicationFacadeBean();
-//		LoggingDTO dto = new LoggingDTO();
-//		dto.setComponent(component);
-//		dto.setDetails(details);
-//		dto.setLevel(level.toString());
-//		dto.setUsername(username);
-//		dto.setNode((String)facade.requestNodeConfigs().get("node"));
-//		loggingProcessingProducerService.produce(dto);
 		System.out.printf("[%s] %s. %s. %s.\n", level.toString(), component, username, details);
 
 	}	
