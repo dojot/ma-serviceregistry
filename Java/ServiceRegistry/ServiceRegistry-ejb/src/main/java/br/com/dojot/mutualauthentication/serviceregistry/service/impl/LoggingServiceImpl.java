ebf3e967574090899e0d5e55cb5b19c2e67ff854
Henrique Canto Duarte <hcantoduarte@gmail.com>
2018-01-02 10:10:09 -0200

    Removed unused imported packages

--- Java/ServiceRegistry/ServiceRegistry-ejb/src/main/java/br/com/dojot/mutualauthentication/serviceregistry/service/impl/LoggingServiceImpl.java
+++ Java/ServiceRegistry/ServiceRegistry-ejb/src/main/java/br/com/dojot/mutualauthentication/serviceregistry/service/impl/LoggingServiceImpl.java
@@ -1,12 +1,7 @@
 package br.com.dojot.mutualauthentication.serviceregistry.service.impl;
 
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-import br.com.dojot.mutualauthentication.communication.facade.api.CommunicationFacade;
-import br.com.dojot.mutualauthentication.communication.facade.impl.CommunicationFacadeBean;
-import br.com.dojot.mutualauthentication.serviceregistry.beans.dto.LoggingDTO;
-//import br.com.dojot.mutualauthentication.serviceregistry.messaging.api.LoggingProcessingProducerService;
 import br.com.dojot.mutualauthentication.serviceregistry.service.api.LoggingService;
 
 @Stateless
